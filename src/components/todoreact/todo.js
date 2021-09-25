c6a7dbd482dc9c93aae4fcc36adb5d0ad43a6f1d
Avneesh Jha <avneeshjha2015@gmail.com>
2021-09-26 01:14:19 +0530

    Update UI

--- src/components/todoreact/todo.js
+++ src/components/todoreact/todo.js
@@ -16,6 +16,20 @@ const Todo = () => {
                     />
                     <i className="fa fa-plus add-btn"></i>
                 </div>
+
+                {/* show our items*/}
+                <div className="showItems">
+                    <div className="eachItem">
+                        <h3>Apple</h3>
+                        <div className="todo-btn">
+                        <i className="far fa-edit add-btn"></i>
+                        <i className="far fa-trash-alt add-btn"></i>
+
+                        </div>
+
+                    </div>
+                </div>
+                {/* sremove all button*/}
                 <div className="showItems">
                     <button className="btn effect04" data-sm-link-text="Remove All">
                      <span> CHECK LIST</span>
